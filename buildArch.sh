45bd50ea333488155edc440b1f9f301ded314d4a
Corbin Champion <corbinlc@gmail.com>
2024-01-05 13:06:45 -0800

    first pass at asset build

--- buildArch.sh
+++ buildArch.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-docker-compose -f main.yml -f $1.yml down
-docker-compose -f main.yml -f $1.yml build
-docker-compose -f main.yml -f $1.yml up
+#install some packages
+apt-get update
+apt-get install -y --no-install-recommends unzip wget
+
+#clean up after ourselves
+apt-get clean
+
+#download and unzip bootstrap for correct release
+mkdir bootstrap
+case "$1" in
+  x86)
+      wget https://github.com/termux/termux-packages/releases/latest/download/bootstrap-i686.zip
+      unzip -d bootstrap bootstrap-i686.zip
+      ;;
+  arm)
+      wget https://github.com/termux/termux-packages/releases/latest/download/bootstrap-arm.zip
+      unzip -d bootstrap bootstrap-arm.zip
+      ;;
+  x86_64)
+      wget https://github.com/termux/termux-packages/releases/latest/download/bootstrap-x86_64.zip
+      unzip -d bootstrap bootstrap-x86_64.zip
+      ;;
+  arm64)
+      wget https://github.com/termux/termux-packages/releases/latest/download/bootstrap-aarch64.zip
+      unzip -d bootstrap bootstrap-aarch64.zip
+      ;;
+  *)
+      echo "unsupported architecture"
+      exit
+      ;;
+esac
+
 mkdir -p release
-cp output/rootfs.tar.gz release/$1-rootfs.tar.gz
+tar -C bootstrap -czvf release/$1-rootfs.tar.gz .
 mkdir -p release/assets
 cp assets/all/* release/assets/
 rm release/assets/assets.txt
 cp output/busybox release/assets/
-cp output/libdisableselinux.so release/assets/
 tar -czvf release/$1-assets.tar.gz -C release/assets/ .
 for f in $(ls release/assets/); do echo "$f $(date +%s -r release/assets/$f) $(md5sum release/assets/$f | awk '{ print $1 }')" >> release/$1-assets.txt; done
 rm -rf release/assets
