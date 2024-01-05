00318921673832b721a78798a07673e9133dd89e
Corbin Champion <corbinlc@gmail.com>
2024-01-05 14:32:46 -0800

    update golang version

--- buildArch.sh
+++ buildArch.sh
@@ -37,7 +37,6 @@ tar -C bootstrap -czvf release/$1-rootfs.tar.gz .
 mkdir -p release/assets
 cp assets/all/* release/assets/
 rm release/assets/assets.txt
-cp output/busybox release/assets/
 tar -czvf release/$1-assets.tar.gz -C release/assets/ .
 for f in $(ls release/assets/); do echo "$f $(date +%s -r release/assets/$f) $(md5sum release/assets/$f | awk '{ print $1 }')" >> release/$1-assets.txt; done
 rm -rf release/assets
